6ae4dce1102d189c442db27e0d404121eda5059b
gustavool <gustavool@live.com>
2022-10-02 19:06:36 -0300

    refactor: card jutsus - flex box to grid responsive

--- src/components/BoxJutsus/styles.ts
+++ src/components/BoxJutsus/styles.ts
@@ -2,10 +2,9 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  margin-top: 16px;
-  column-gap: 4px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(160px, 2fr));
+  grid-gap: 8px;
+  margin: 8px auto;
+  justify-items: center;
 `;
--- src/components/JutsuCard/styles.ts
+++ src/components/JutsuCard/styles.ts
@@ -2,16 +2,14 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   width: 100%;
-  max-width: 160px;
+  max-width: 256px;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  margin: 8px auto;
 `;
 
 export const BoxImage = styled.div`
   width: 100%;
-  height: auto;
   img {
     border-radius: 4px 4px 0 0;
     width: 100%;
@@ -20,13 +18,12 @@ export const BoxImage = styled.div`
 
 export const Name = styled.span`
   width: 100%;
-  max-width: 160px;
+  height: 24px;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: ${({ theme }) => theme.background.primary};
   border-radius: 0 0 4px 4px;
-  height: 24px;
   font-size: 14px;
   font-weight: bold;
   margin-top: -4px;
